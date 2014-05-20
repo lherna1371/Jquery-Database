f0d7db116c8b7194239d37df82bc560d0a834078
Luis Hernandez <lhernandez@colum.edu>
2014-05-20 16:18:00 -0500

    event/activity creation is active

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
 class WelcomeController < ApplicationController
   def index
   
-  @activity_list = Activity.all
-  puts @activity_list.inspect
-  	
+  @activity_map = []
+  list = Activity.all
+  list.each do |activity|
+  	single_list = []
+  	single_list << activity.name
+  	single_list << activity.latitude
+  	single_list << activity.longitude
+  	@activity_map << single_list
+  end 
+  
+  puts "######"
+  puts @activity_map.inspect
   end
   
 end
